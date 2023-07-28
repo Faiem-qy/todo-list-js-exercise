6df0360d7cf53235d797f28516d3e0c85be1699a
Faiem-qy <f.quanyoung@gmail.com>
2023-07-27 21:49:19 -0400

    edit

--- index.js
+++ index.js
@@ -45,13 +45,16 @@ function newTask(title, description) {
 }
 
 
-
-
 // DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
+
 
 // newTask("Clean Cat Litter"); // task 0
 // newTask("Do Laundry"); // task 1
@@ -62,6 +65,3 @@ const tasks = [task1, task2];
 
 // console.log(tasks);
 
-task1.logState();
-task1.markCompleted();
-task1.logState();
