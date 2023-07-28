1e0b14cb1052420712d82d48f390a6f64527db97
Faiem-qy <f.quanyoung@gmail.com>
2023-07-27 21:50:28 -0400

    edit

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// Arrays to keep track of each task's state
-// const taskTitles = [];
-// const taskComplete = [];
-// const taskDescriptions = [];
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-// function newTask(title, description) {
-//   taskTitles.push(title);
-//   taskDescriptions.push(description)
-//   taskComplete.push(false);
-// }
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-// function completeTask(taskIndex) {
-//   taskComplete[taskIndex] = true;
-// }
-
-
-// Print the state of a task to the console in a nice readable way
-// function logTaskState(taskIndex) {
-//   const title = taskTitles[taskIndex];
-//   const complete = taskComplete[taskIndex];
-//   console.log(`${title} has${complete ? " " : " not "}been completed`);
-// }
-
-
-
-
 function newTask(title, description) {
   const task = {
     title: title,
@@ -54,6 +25,33 @@ const tasks = [task1, task2];
 task1.logState();
 task1.markCompleted();
 task1.logState();
+
+
+// Arrays to keep track of each task's state
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
+
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
+// function newTask(title, description) {
+//   taskTitles.push(title);
+//   taskDescriptions.push(description)
+//   taskComplete.push(false);
+// }
+
+// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+// function completeTask(taskIndex) {
+//   taskComplete[taskIndex] = true;
+// }
+
+
+// Print the state of a task to the console in a nice readable way
+// function logTaskState(taskIndex) {
+//   const title = taskTitles[taskIndex];
+//   const complete = taskComplete[taskIndex];
+//   console.log(`${title} has${complete ? " " : " not "}been completed`);
+// }
 
 
 // newTask("Clean Cat Litter"); // task 0
